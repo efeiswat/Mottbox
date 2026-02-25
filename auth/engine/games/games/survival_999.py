6dbc8047c9192f63f237f4eae0e719c49d3af4c9
efeiswat <efesdfhesbl59@gmail.com>
2026-02-25 22:52:01 +0300

    Create survival_999.py

---
+++ auth/engine/games/games/survival_999.py
@@ -0,0 +1,10 @@
+class Survival999:
+    def __init__(self, user):
+        self.user = user
+
+    def start_night(self):
+        night = input("Kaçıncı geceden başlamak istersin? (1-999): ")
+        print(f"Gece {night} başlıyor... Sis yoğunlaşıyor.")
+        print("GÖREV: Ormanda çocuğu bul ve sığınağı koru!")
+        print("İpucu: Madalyonu bulmak için kulübenin arkasına bak.")
+      
