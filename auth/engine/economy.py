b5ec49816e016a6b66d9ee292a0abb9152c07067
efeiswat <efesdfhesbl59@gmail.com>
2026-02-26 22:14:57 +0300

    Delete auth directory

--- auth/engine/economy.py
+++
@@ -1,6 +0,0 @@
-class Economy:
-    def transfer_ui(self, sender):
-        target = input("Alıcı Kullanıcı Adı: ")
-        amount = int(input("Miktar: "))
-        print(f"İŞLEM BAŞARILI: {amount} Mats Coin {target} hesabına aktarıldı.")
-      
